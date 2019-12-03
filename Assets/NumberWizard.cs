e1f489ba5ea7a7f5fac8551d43f8fcaeec8e1a0b
ramarier11 <ramarier11@gmail.com>
2019-12-03 17:16:01 -0500

    Finished Up Program

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -16,6 +16,7 @@ public class NumberWizard : MonoBehaviour
     Debug.Log("And the lowest number you can pick is: " + min);
     Debug.Log("Tell me if your number is higher or lower than 500.");
     Debug.Log("Push Up Arrow for higher, Push Down Arrow for lower, Push Enter for Correct");
+    max = max + 1;
   }
 
   // Update is called once per frame
@@ -24,15 +25,15 @@ public class NumberWizard : MonoBehaviour
     //Detect when the up arrow key is pressed down
     if (Input.GetKeyDown(KeyCode.UpArrow))
     {
-      Debug.Log("Up Arrow key was pressed.");
       min = guess;
-      Debug.Log(guess);
+      guess = (max + min) / 2;
+      Debug.Log("Ok how about " + guess);
     }
     else if (Input.GetKeyDown(KeyCode.DownArrow))
     {
-      Debug.Log("Down Arrow key was pressed.");
       max = guess;
-      Debug.Log(guess);
+      guess = (max + min) / 2;
+      Debug.Log("Hmm... I got it! " + guess);
     }
     else if (Input.GetKeyDown(KeyCode.Return))
     {
